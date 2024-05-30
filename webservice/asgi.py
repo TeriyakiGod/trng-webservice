98238aeb3e9ef76682d46c654521033207d06f1d
Kacper Akdag-Ochnik <kacperochnik01@gmail.com>
2024-05-30 20:56:51 +0200

    allow all wss hosts (for now)

--- webservice/asgi.py
+++ webservice/asgi.py
@@ -2,7 +2,7 @@ import os
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
+from channels.security.websocket import OriginValidator
 from django.core.asgi import get_asgi_application
 
 from trng.urls import urlpatterns
@@ -15,8 +15,9 @@ django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
-        "websocket": AllowedHostsOriginValidator(
-            AuthMiddlewareStack(URLRouter(urlpatterns))
+        "websocket": OriginValidator(
+            AuthMiddlewareStack(URLRouter(urlpatterns)),
+            ['*'] # This is a list of trng ip addresses, for now allow all
         ),
     }
 )
