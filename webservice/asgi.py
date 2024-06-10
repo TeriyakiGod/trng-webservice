cdd7509763541ceb493128942a4c64a2bf55f667
Kacper Ochnik <kacperochnik01@gmail.com>
2024-06-10 22:57:47 +0200

    import fix

--- webservice/asgi.py
+++ webservice/asgi.py
@@ -1,16 +1,15 @@
 import os
-
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import OriginValidator
 from django.core.asgi import get_asgi_application
-
-from trng.urls import urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webservice.settings")
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import OriginValidator
+from trng.urls import urlpatterns
 
 application = ProtocolTypeRouter(
     {
