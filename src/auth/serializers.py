e83bc5f752c1a0179edfe4b1127903cbb9d50823
Kacper Akdag-Ochnik <kacperochnik01@gmail.com>
2025-03-01 14:24:18 +0100

    fixed auth login, register, email confirmation

--- src/auth/serializers.py
+++ src/auth/serializers.py
@@ -1,21 +1,22 @@
 from rest_framework import serializers
 from django.contrib.auth.password_validation import validate_password
-
+from django.core.exceptions import ValidationError
 
 class UserRegisterSerializer(serializers.Serializer):
-    username = serializers.CharField(required=True, min_length=5,max_length=30, help_text="All alphanumeric characters allowed, must be between 5 and 30 characters long.", style={'input_type': 'text'})
-    email = serializers.EmailField(required=True, help_text="Enter a valid email address.",style={'input_type': 'email'})
+    username = serializers.CharField(required=True, min_length=5,max_length=30, style={'input_type': 'text'})
+    email = serializers.EmailField(required=True, style={'input_type': 'email'})
     password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
     confirm_password = serializers.CharField(required=True, write_only=True,style={'input_type': 'password'})
 
     def validate(self, data):
         if data['password'] != data['confirm_password']:
             raise serializers.ValidationError({"password": "Passwords do not match."})
-        # It's important to validate the password field using the validate_password method
-        validate_password(data['password'])
+        try:
+            validate_password(data['password'])
+        except ValidationError as e:
+            raise serializers.ValidationError({"password": e.messages})
         return data
     
 class UserLoginSerializer(serializers.Serializer):
-    email = serializers.EmailField(required=True)
+    username = serializers.CharField(required=True)
     password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
-    
